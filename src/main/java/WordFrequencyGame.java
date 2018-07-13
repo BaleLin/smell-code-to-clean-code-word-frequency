3f3e904365362637a3e29c81d3853b068ff4d6ad
BaleLin <bale.lin@oocl.com>
2018-07-13 15:40:51 +0800

    完整重构代码

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -10,33 +10,48 @@ public class WordFrequencyGame {
             return inputStr + " 1";
         } else {
 
-            String[] arr = inputStr.split("\\s+");
-            List<Input> inputList = new ArrayList<>();
-            //split the input string with 1 to n pieces of spaces
-            for (String s : arr) {
-                Input input = new Input(s, 1);
-                inputList.add(input);
-            }
+            Map<String, List<Input>> map = groupInputWord(inputStr);
 
-            //get the map for the next step of sizing the same word
-            Map<String, List<Input>> map = getListMap(inputList);
+            List<Word> resultModle = tanslateToModle(map);
 
-            List<Input> list = new ArrayList<>();
-            for (Map.Entry<String, List<Input>> entry : map.entrySet()) {
-                Input input = new Input(entry.getKey(), entry.getValue().size());
-                list.add(input);
-            }
+            resultModle.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
 
-            list.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
-
-            StringJoiner joiner = new StringJoiner("\n");
-            for (Input w : list) {
-                String s = w.getValue() + " " + w.getWordCount();
-                joiner.add(s);
-            }
-            return joiner.toString();
+            StringJoiner resultView = buildResultView(resultModle);
+            return resultView.toString();
 
         }
+    }
+
+    private StringJoiner buildResultView(List<Word> resultModle) {
+        StringJoiner resultView = new StringJoiner("\n");
+        for (Word w : resultModle) {
+            String s = w.getValue() + " " + w.getWordCount();
+            resultView.add(s);
+        }
+        return resultView;
+    }
+
+    private List<Word> tanslateToModle(Map<String, List<Input>> map) {
+        List<Word> resultModle = new ArrayList<>();
+        for (Map.Entry<String, List<Input>> entry : map.entrySet()) {
+            Word input = new Word(entry.getKey(), entry.getValue().size());
+            resultModle.add(input);
+
+        }
+        return resultModle;
+    }
+
+    private Map<String, List<Input>> groupInputWord(String inputStr) {
+        String[] arr = inputStr.split("\\s+");
+        List<Input> inputList = new ArrayList<>();
+        //split the input string with 1 to n pieces of spaces
+        for (String s : arr) {
+            Input input = new Input(s, 1);
+            inputList.add(input);
+        }
+
+        //get the map for the next step of sizing the same word
+        return getListMap(inputList);
     }
 
     private Map<String, List<Input>> getListMap(List<Input> inputList) {
